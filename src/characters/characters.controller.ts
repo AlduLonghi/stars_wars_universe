0e604dc1796a0b49d9ad0240109d352b402f0921
Aldana Longhi <aldulonghi@gmail.com>
2023-09-15 14:30:45 -0300

    add validator pipe

--- src/characters/characters.controller.ts
+++ src/characters/characters.controller.ts
@@ -18,17 +18,17 @@ export class CharactersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.charactersService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.charactersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCharacterDto: UpdateCharacterDto) {
-    return this.charactersService.update(+id, updateCharacterDto);
+  update(@Param('id') id: number, @Body() updateCharacterDto: UpdateCharacterDto) {
+    return this.charactersService.update(id, updateCharacterDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.charactersService.remove(+id);
+  remove(@Param('id') id: number) {
+    return this.charactersService.remove(id);
   }
 }
